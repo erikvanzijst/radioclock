e9afc21f379b125e3a2501fd3583b73daca083ff
Erik van Zijst <erik.van.zijst@gmail.com>
2022-06-29 15:15:33 +0200

    Add CRC8 checksum verification to I2C reads

--- firmware/main.c
+++ firmware/main.c
@@ -9,6 +9,22 @@ typedef struct {
     uint8_t crc;
 } dht_measurement_t;
 
+uint8_t crc8(uint8_t *ptr, uint8_t len) {
+    uint8_t crc = 0xFF;
+    while (len--) {
+        crc ^= *ptr++;
+        for (uint8_t i = 0; i < 8; i++) {
+            if (crc & 0x80) {
+                crc <<= 1;
+                crc ^= 0x31;
+            } else {
+                crc <<= 1;
+            }
+        }
+    }
+    return crc;
+}
+
 int main(void)
 {
     struct io_descriptor *io;
@@ -40,12 +56,17 @@ int main(void)
         io_write(I2C_0_io, (uint8_t*)((uint8_t []){0xac, 0x33, 0x0}), 3);
         delay_ms(100);  // wait for the sensor to acquire a measurement
         io_read(I2C_0_io, (uint8_t *)&measurement, sizeof (measurement));
+
         if ((measurement.status & 0x1) == 0x0) {
             int32_t tmp = measurement.data[4] + (measurement.data[3] << 8) + ((measurement.data[2] & 0xf) << 16);
             int temperature = (((tmp * 2000) / 1048576) - 500);
             printf("Temperature: %d.%dC\r\n", temperature / 10, temperature % 10);
+
+        } else if (measurement.crc != crc8((uint8_t *)&measurement, 6)) {
+            printf("ERR: DHT20 I2C CRC mismatch (%d != %d)\r\n", measurement.crc, crc8((uint8_t *)&measurement, 6));
+
         } else {
-            printf("DHT20 I2C reading failed (%x)\r\n", measurement.status);
+            printf("ERR: DHT20 I2C reading failed (status: %x)\r\n", measurement.status);
         }
         delay_ms(1000);
     }
