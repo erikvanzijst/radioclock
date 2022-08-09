5b77b91c16632c00348c1324f12e25757d0e6e12
Erik van Zijst <erik.van.zijst@gmail.com>
2022-08-09 21:40:37 +0200

    Fix I2C reinitialization bug after STANDBY

--- firmware/src/dht.c
+++ firmware/src/dht.c
@@ -16,7 +16,7 @@ typedef struct {
 int32_t temperature;    // in celsius * 10 (to avoid float formatting later on)
 int32_t humidity;
 
-struct io_descriptor *I2C_0_io;
+volatile struct io_descriptor *I2C_0_io = NULL;
 static struct timer_task TIMER_0_trigger_task, TIMER_0_read_task;
 volatile dht_measurement_t measurement;
 static uint8_t CMD_START[] = {0xac, 0x33, 0x0};     // command to start a measurement
@@ -32,8 +32,9 @@ int32_t get_humidity() {
 
 void read_measurement(const struct timer_task *const timer_task) {
     if (tx_timestamp && (millis() - tx_timestamp >= DHT_RESPONSE_TIME)) {
-        if (io_read(I2C_0_io, (uint8_t *)&measurement, sizeof (measurement)) < 0) {
-            ulog(ERROR, "io_read() failed")
+        int32_t err;
+        if ((err = io_read(I2C_0_io, (uint8_t *)&measurement, sizeof (measurement))) < 0) {
+            ulog(ERROR, "io_read() failed (%ld)", (unsigned long)err)
         }
         tx_timestamp = 0;
     }
@@ -44,7 +45,10 @@ void tx_complete(struct i2c_m_async_desc *const i2c) {
 }
 
 void start_measurement(const struct timer_task *const timer_task) {
-    io_write(I2C_0_io, (uint8_t*)CMD_START, sizeof(CMD_START));
+    int32_t err;
+    if ((err = io_write(I2C_0_io, (uint8_t*)CMD_START, sizeof(CMD_START))) < 0) {
+        ulog(ERROR, "io_write() failed (%ld)", (unsigned long)err)
+    }
 }
 
 void rx_complete(struct i2c_m_async_desc *const i2c) {
@@ -64,26 +68,33 @@ void rx_complete(struct i2c_m_async_desc *const i2c) {
 }
 
 int32_t dht_init(void) {
+    int32_t err;
 
-    i2c_m_async_get_io_descriptor(&I2C_0, &I2C_0_io);
-    i2c_m_async_enable(&I2C_0);
-    i2c_m_async_set_slaveaddr(&I2C_0, 0x38, I2C_M_SEVEN);
+    if (I2C_0_io == NULL) {
+        i2c_m_async_get_io_descriptor(&I2C_0, &I2C_0_io);
+        if ((err = i2c_m_async_enable(&I2C_0)) < 0) {
+            ulog(ERROR, "i2c_m_async_enable() failed (%ld)", (unsigned long)err)
+        }
+        i2c_m_async_set_slaveaddr(&I2C_0, 0x38, I2C_M_SEVEN);
+
+        i2c_m_async_register_callback(&I2C_0, I2C_M_ASYNC_TX_COMPLETE, (FUNC_PTR)tx_complete);
+        i2c_m_async_register_callback(&I2C_0, I2C_M_ASYNC_RX_COMPLETE, (FUNC_PTR)rx_complete);
+
+        TIMER_0_trigger_task.interval   = 500;
+        TIMER_0_trigger_task.cb         = start_measurement;
+        TIMER_0_trigger_task.mode       = TIMER_TASK_REPEAT;
+
+        TIMER_0_read_task.interval      = 100;       // collect measurement
+        TIMER_0_read_task.cb            = read_measurement;
+        TIMER_0_read_task.mode          = TIMER_TASK_REPEAT;
+    }
 
     // DHT20 initialization
     delay_ms(100);  // Wait for DHT20 module to initialize
-    io_write(I2C_0_io, (uint8_t*)((uint8_t []){0xba}), 1);  // soft reset command
+    if ((err = io_write(I2C_0_io, (uint8_t*)((uint8_t []){0xba}), 1)) < 0) {    // soft reset command
+        ulog(ERROR, "io_write() failed on soft reset cmd 0xba (%ld)", (unsigned long)err)
+    }
     delay_ms(20);  // wait for the sensor reset
-
-    i2c_m_async_register_callback(&I2C_0, I2C_M_ASYNC_TX_COMPLETE, (FUNC_PTR)tx_complete);
-    i2c_m_async_register_callback(&I2C_0, I2C_M_ASYNC_RX_COMPLETE, (FUNC_PTR)rx_complete);
-
-    TIMER_0_trigger_task.interval   = 500;
-    TIMER_0_trigger_task.cb         = start_measurement;
-    TIMER_0_trigger_task.mode       = TIMER_TASK_REPEAT;
-
-    TIMER_0_read_task.interval      = 100;       // collect measurement
-    TIMER_0_read_task.cb            = read_measurement;
-    TIMER_0_read_task.mode          = TIMER_TASK_REPEAT;
 
     if (timer_add_task(&TIMER_0, &TIMER_0_trigger_task) ||
         timer_add_task(&TIMER_0, &TIMER_0_read_task)) {
@@ -93,20 +104,10 @@ int32_t dht_init(void) {
 }
 
 int32_t dht_deinit(void) {
-    if (i2c_m_async_register_callback(&I2C_0, I2C_M_ASYNC_TX_COMPLETE, (FUNC_PTR)NULL) ||
-        i2c_m_async_register_callback(&I2C_0, I2C_M_ASYNC_RX_COMPLETE, (FUNC_PTR)NULL)) {
-        ulog(ERROR, "i2c_m_async_register_callback() failed")
-        return -1;
-    }
 
     if (timer_remove_task(&TIMER_0, &TIMER_0_read_task) ||
         timer_remove_task(&TIMER_0, &TIMER_0_trigger_task)) {
         ulog(ERROR, "timer_remove_task() failed")
-        return -1;
-    }
-
-    if (i2c_m_async_disable(&I2C_0)) {
-        ulog(ERROR, "i2c_m_async_disable() failed")
         return -1;
     }
     return ERR_NONE;
